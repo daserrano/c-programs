643721235257d1ed0365faf0d6920e19cc001f79
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-01-21 12:03:43 +0100

    three statement

--- solutions/03_mean2/mean2.cpp
+++ solutions/03_mean2/mean2.cpp
@@ -5,21 +5,23 @@
 
 int main(int argc, char *argv[]){
 
-int numero=0, resultado=0, contador=0;
+int numero=0, contador=0;
+double resultado =0;
 do{
 
 printf("Introduce un numero: ");
 scanf(" %i", &numero);
 
+if(numero > 0){
 resultado += numero;
-contador += contador;
-
+contador++;
+}
 
 }while(numero > 0);
 
 resultado /= contador;
 
-printf("La media de todos los numeros es %i \n", resultado);
+printf("La media de todos los numeros es %.2lf \n", resultado);
 
 
 
