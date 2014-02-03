1a3d3ebe28f7c200c035f51ab3b53e8db5b3737d
David <david@david-VirtualBox.(none)>
2014-02-03 23:07:35 +0100

    statement number four and five

---
+++ solutions/04_triangulo/triangulo.cpp
@@ -0,0 +1,32 @@
+//Given the height, build a triangle as in the example with the character provided in the invocation.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+    // Si no se introduce un numero, que muestre por pantalla un error.
+    if(argc<2){
+	fprintf(stderr, "Please, introduce a number.\n");
+	exit (EXIT_FAILURE);
+    }
+
+    // Se indica que la variable numero tendra el valor del parametro.
+    int numero = atoi(argv[1]);
+
+    // Se crea una matriz de 2x2
+    for(int fila=0; fila < numero ; fila++){
+	for(int columna=0; columna < numero; columna++){
+	    // Si la fila es mayor o igual que la columna pintamos asteriscos, si no, no
+	    if(fila>= columna)
+		printf("*");
+	    else 
+		printf(" ");
+
+	}
+	printf("\n");
+    }
+
+    EXIT_SUCCESS;
+}
