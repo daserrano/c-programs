50a1592bb82b858090287e3fe9e884de13f66c60
David <david@david-VirtualBox.(none)>
2014-02-10 14:07:39 +0100

    correction of the first statement

--- solutions/01_simple_output/repeat.cpp
+++ solutions/01_simple_output/repeat.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[2]){
+int main(int argc, char *argv[1]){
 
-if(argc < 2) 
+if(argc < 2)
 printf("Please, you must introduce two words.\n");
 
 else
