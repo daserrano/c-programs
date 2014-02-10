7004bcba53b13b2ef069b4b4771ad3ac419a105d
David <david@david-VirtualBox.(none)>
2014-02-10 21:13:01 +0100

    the eight statement is incomplete.

---
+++ solutions/07_adding/adding.cpp
@@ -0,0 +1,50 @@
+/*Define a funtion with the following signature:
+
+  ```c
+  double add(double,  double);
+  ```
+
+  Function _main_ shall fetch operands from command line.
+
+
+  ```bash
+  the_owl@eniac:~$ ./add 2.3 5.8
+  ```
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fallo(char *argv[]){
+
+    fprintf(stderr, "Usage: %s number number \n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+double add(double numero1, double numero2){
+
+    double resultado=0;
+
+    resultado = numero1 + numero2;
+    return resultado;
+}
+
+int main(int argc, char *argv[]){
+
+    // Si no se introducen 2 numeros, llamada a la funcion para que indique el error y su forma de uso.
+    if(argc<3)
+	fallo(argv);
+
+    // Atof para convertir los datos en tipo float.
+    double numero1 = atof(argv[1]), 
+	   numero2 = atof(argv[2]), 
+	   resultado = 0;
+
+    // La variable resultado tiene el valor que le devuelva la funcion add.
+    resultado = add(numero1, numero2);
+
+    printf(" %.2lf + %.2lf = %.2lf \n", numero1, numero2, resultado); 
+
+    return EXIT_SUCCESS;
+}
---
+++ solutions/08_adding/adding.cpp
@@ -0,0 +1,36 @@
+// Arrange the _main_ function of problem number 07 to accept a variable number of arguments.
+
+
+
+
+
+// SIN TERMINAR.
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double add(double numeros[argc-1]){
+
+    double resultado=0;
+
+    return resultado;
+}
+
+int main(int argc, char *argv[]){
+
+double numeros[argc-1],
+       numero_de_numeros = argc-1;
+
+for(int contador=0; contador < numero_de_numeros ; contador++){
+   numeros[contador] = atof(argv[contador+1];
+
+}
+resultado=
+    printf(" %.2lf \n", resultado); 
+
+    return EXIT_SUCCESS;
+}
