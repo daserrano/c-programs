a5bda1e27cad7baf6bd9fb7096b278d832c0530f
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-02-11 09:33:04 +0100

    eight statement did it

--- solutions/08_adding/adding.cpp
+++ solutions/08_adding/adding.cpp
@@ -1,35 +1,30 @@
 // Arrange the _main_ function of problem number 07 to accept a variable number of arguments.
 
 
-
-
-
-// SIN TERMINAR.
-
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-double add(double numeros[argc-1]){
+double add(double numeros[], double numero_de_numeros){
 
     double resultado=0;
 
+    for(int contador=0; contador < numero_de_numeros ; contador++)
+
+	resultado += numeros[contador]; 
     return resultado;
 }
 
 int main(int argc, char *argv[]){
 
-double numeros[argc-1],
-       numero_de_numeros = argc-1;
+    double numeros[argc-1],
+	   numero_de_numeros = argc-1,
+	   resultado=0;
 
-for(int contador=0; contador < numero_de_numeros ; contador++){
-   numeros[contador] = atof(argv[contador+1];
+    for(int contador=0; contador < numero_de_numeros ; contador++){
+	numeros[contador] = atof(argv[contador+1]);
 
-}
-resultado=
+    }
+    resultado = add(numeros, numero_de_numeros);
     printf(" %.2lf \n", resultado); 
 
     return EXIT_SUCCESS;
