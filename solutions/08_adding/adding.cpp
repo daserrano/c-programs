3ebdfa09b358f66cdd2ac86f1086eb3a3222b326
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-02-11 09:40:25 +0100

    eight statement did it with comments

--- solutions/08_adding/adding.cpp
+++ solutions/08_adding/adding.cpp
@@ -7,7 +7,7 @@
 double add(double numeros[], double numero_de_numeros){
 
     double resultado=0;
-
+// Se suma todo en la variable resultado y se va acumulando
     for(int contador=0; contador < numero_de_numeros ; contador++)
 
 	resultado += numeros[contador]; 
@@ -16,14 +16,17 @@ double add(double numeros[], double numero_de_numeros){
 
 int main(int argc, char *argv[]){
 
+    //argc-1 para introducir tantos numeros como se quiera.
     double numeros[argc-1],
 	   numero_de_numeros = argc-1,
 	   resultado=0;
 
+    // Se meten todos los datos en una matriz.
     for(int contador=0; contador < numero_de_numeros ; contador++){
 	numeros[contador] = atof(argv[contador+1]);
 
     }
+    //Llamada a la funcion.
     resultado = add(numeros, numero_de_numeros);
     printf(" %.2lf \n", resultado); 
 
