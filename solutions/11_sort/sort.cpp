eb944fd1f9ae15024bd5c10b87f3126df46eb385
David <david@david-VirtualBox.(none)>
2014-02-14 18:47:33 +0100

    sort statement done it.

--- solutions/11_sort/sort.cpp
+++ solutions/11_sort/sort.cpp
@@ -12,21 +12,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-char comparar_palabras(const char *list){
+char comparar_palabras(const char *list[]){
 
     int compara=0;
-    char *primera = list[0], //Da error de conversion.
-	 *segunda = list[1];
-
+    const char *primera,
+	  *segunda;
+    primera = list[0];
+    segunda = list[1];
     compara = strcmp(primera, segunda);
+    printf("%i \n", compara);
     if(compara == 0)
 	printf("Las palabras son iguales.");
     else{
-	if(compara < 0) 
-	    printf(" %c %c", list[1], list[0]);
+	if(compara > 0) 
+	    printf(" %s %s", list[1], list[0]);
 	else
-	    printf(" %c %c", list[0], list[1]);
+	    printf(" %s %s", list[0], list[1]);
     }
+    printf("\n");
 }
 
 
@@ -34,7 +37,7 @@ int main(int argc, char *argv[]){
 
     const char *list[] = { "yield", "yelp"};
 
-    comparar_palabras(*list);
+    comparar_palabras(list);
 
     return EXIT_SUCCESS;
 }
