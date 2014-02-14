11df493fc16d6998d2d6d5b7f1b894e8f682667d
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-02-14 10:45:02 +0100

    prueba de sort

---
+++ solutions/11_sort/pruebas.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+    int compara=0;
+    const  char *nombre1= "abecedario", *nombre2="a";
+
+
+    compara = strcmp(nombre1, nombre2);
+
+    if(compara==0)
+	printf("Son iguales \n");
+    else{
+	if(compara > 0)
+	    printf("%s %s \n", nombre2, nombre1);
+	else
+	    printf("%s %s \n", nombre1, nombre2);
+    }
+    return EXIT_SUCCESS;
+}
