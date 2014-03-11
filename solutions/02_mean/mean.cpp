101257a5bf9faba67a09505b0392ed7253ae972b
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-03-11 11:18:27 +0100

    update in some ejercices

--- solutions/02_mean/mean.cpp
+++ solutions/02_mean/mean.cpp
@@ -5,17 +5,17 @@ int main(int argc, char *argv[1]){
 
 double numero, media;
 
-for(int contador=1; contador <= 10; contador++){
+for(int contador=1; contador <= 10; contador++){ //Pide 10 numeros.
 
 printf("%i. Introduce un numero: ", contador);
 scanf(" %lf", &numero);
 
-media += numero;
+media += numero; //Almacena los numeros en media.
 
 }
 
-media /= 10;
-printf("La media es %.2lf \n", media);
+media /= 10.;  //Lleva . para indicar un double.
+printf("La media es %.2lf \n", media); 
 
 return EXIT_SUCCESS;
 }
