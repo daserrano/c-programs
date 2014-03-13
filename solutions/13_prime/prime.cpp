0f8e487868ac96dcd85a26cefe9e9f82f2c5f4c4
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-03-13 10:12:04 +0100

    13rd statement

--- solutions/13_prime/prime.cpp
+++ solutions/13_prime/prime.cpp
@@ -1,5 +1,7 @@
 /* given number is prime when is not is divided by the lower primes. Find the 70 first primes. */
 
+// Try Criba de Eratóstenes.
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,30 +16,31 @@ int main(int argc, char *argv[]){
     int primes_found = 1,
 	possible_prime = 3;
 
-    bool is_divisible = false;
-
+    bool divisible = false;  // Para comprobar si un numero tiene numeros divisibles.
     primes[0] = 2;
 
     do{
 
-	for(int divisor=2; !is_divisible && (divisor <= possible_prime/2); divisor++, possible_prime++){
+	for(int divisor=2; divisor < possible_prime; divisor++){
 
-	    if(possible_prime % divisor == 0)
-		is_divisible = true;
-
-	    else{
-		primes[primes_found] = possible_prime;
-		primes_found++;
-
+	    if(possible_prime % divisor == 0){ // Si es divisible sale del for.
+		divisible = true;
+		break;
 	    }
-
-	    is_divisible = false;
 	}
 
-    }while(primes_found <= N);
+	if(divisible == false){ // Si no es divisible, entonces es un numero primo.
+	    primes[primes_found] = possible_prime;
+	    primes_found++;
+	}
+	divisible = false;
+	possible_prime++;
 
-    for(int contador=0; contador < N; contador++)
-	printf("%i ", primes[contador]);
+
+    }while(primes_found <= N); // Repetir todo hasta que no encuentre 70 numeros primos.
+
+    for(int contador=0; contador < N; contador++) // Imprimir la matriz.
+	printf("%i ", primes[contador]); 
 
 
     printf("\n");
