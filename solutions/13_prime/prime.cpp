8d55fca9d283831ccb56228a8369a3b51aa1549b
David <david@david-VirtualBox.(none)>
2014-03-13 20:17:04 +0100

    prime with functions

--- solutions/13_prime/prime.cpp
+++ solutions/13_prime/prime.cpp
@@ -9,10 +9,30 @@
 
 
 
+void imprimir(int primes[N]){
+
+    for(int contador=0; contador < N; contador++) // Imprimir la matriz.
+	printf("%i ", primes[contador]); 
+
+    printf("\n");
+}
+
+void rellenar(int primes[N], int primes_found, int possible_prime){
+
+    primes[primes_found] = possible_prime;
+
+} 
+
+
+int is_divisible(int possible_prime, int divisor){
+
+    return possible_prime % divisor == 0;
+
+}
+
 int main(int argc, char *argv[]){
 
     int primes[N];
-
     int primes_found = 1,
 	possible_prime = 3;
 
@@ -23,15 +43,15 @@ int main(int argc, char *argv[]){
 
 	for(int divisor=2; divisor < possible_prime; divisor++){
 
-	    if(possible_prime % divisor == 0){ // Si es divisible sale del for.
+	    if(is_divisible(possible_prime, divisor)){ // Si es divisible sale del for.
 		divisible = true;
 		break;
 	    }
 	}
 
 	if(divisible == false){ // Si no es divisible, entonces es un numero primo.
-	    primes[primes_found] = possible_prime;
-	    primes_found++;
+	    rellenar(primes, primes_found, possible_prime);
+            primes_found++;
 	}
 	divisible = false;
 	possible_prime++;
@@ -39,11 +59,7 @@ int main(int argc, char *argv[]){
 
     }while(primes_found <= N); // Repetir todo hasta que no encuentre 70 numeros primos.
 
-    for(int contador=0; contador < N; contador++) // Imprimir la matriz.
-	printf("%i ", primes[contador]); 
-
-
-    printf("\n");
+    imprimir(primes);
 
     return EXIT_SUCCESS;
 }
