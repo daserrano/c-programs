8b06955d1e36bff314891822c05f4fc254e37540
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-03-18 11:17:02 +0100

    update of print_backwards

---
+++ solutions/10_print_backwards/print_backwards2.0.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+
+    const char *sentence = "The world is a vampire.";
+    const char *beginning = sentence;
+    const char *end = sentence;
+
+
+    do{
+	*end++;
+
+    }while(*end != '\0');
+
+    do{
+	*end--;
+	printf("%c", *end);
+
+    }while(*end != *beginning);
+
+
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
