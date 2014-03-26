c25e549d732af8cad713ef7b2f31126263f4c075
David <david@david-VirtualBox.(none)>
2014-03-26 21:16:52 +0100

    more of getopt

--- solutions/a09_raise_to/raise_to2.0.cpp
+++ solutions/a09_raise_to/raise_to2.0.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <strings.h>
 
 #define ENGLISH 0
 #define SPANISH 1
+#define MAX 255
 
 void usage(const char *argv){
 
-    fprintf(stderr, "------------------\n"
-	    	    "\tUSAGE \n"
-		    "\n"
-		    "%s <base> <exponent>\n", argv);
-	    	    
-
+    fprintf(stderr, "\tUSAGE \n"
+	    "\n"
+	    "%s <base> <exponent>\n", argv);
 
 }
 
@@ -27,36 +27,48 @@ double operacion(double base, int exponent){
 
 }
 
-
 int main(int argc, char *argv[]){
 
-     double base = atof(argv[1]);
-     int exponent = atoi(argv[2]),
-	 c,
-	 language = ENGLISH;
+    double base;
+    int exponent,
+	c,
+	language = ENGLISH;
+	
+    char user[MAX];
 
-     const char *user_language = NULL;
+    const char *user_language = NULL;
 
-     const char *sentence[] = {
-	 			"The result is: ",
-				"El resultado es: "
-     };
+    const char *sentence[] = {
+	"The result is: ",
+	"El resultado es: "
+    };
 
-     while ((c = getopt(argc, argv, "h:")) != -1)
-	    switch(c){
-		case 'h':
-		    usage(argv[0]);
-		    break;
-		
-		case 'l':
-		    user_language = optarg;
-		    //FALTA
-		    if(strcasecmp(user_language, "spanish") == 0)
-			language = SPANISH;
-		    break;
-	    } 
+    while ((c = getopt(argc, argv, "hl:")) != -1)
+	switch(c){
+	    case 'h':
+		usage(argv[0]);
+		break;
 
-     printf("%lf \n", operacion(base, exponent));
+	    case 'l':
+		user_language = optarg;
+		strncpy(user, user_language, MAX); // Sirve para metodo de seguridad. Y el usuario no pueda meter mas de 255 caracteres.
+		if(strcasecmp(user_language, "spanish") == 0) // Sirve para no distinguir entre mayusculas y minusculas.
+		    language = SPANISH;
+		break;
+	    case '?':
+		usage(argv[0]);
+		break;
+	} 
+
+	if (argc - optind < 2){
+            usage(argv[0]);
+	    exit(EXIT_FAILURE);
+	}
+
+	base = atof(argv[optind]);
+	exponent = atoi(argv[optind + 1]);
+
+    printf("%s %lf \n",sentence[language], operacion(base, exponent));
 
 
     return EXIT_SUCCESS;
