e451b5dbbd61b68ab8134ed689edfbcd1975f406
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-03-26 13:32:22 +0100

    update

--- solutions/a_primes/primes.cpp
+++ solutions/a_primes/primes.cpp
@@ -8,26 +8,6 @@
 
 int main(int argc, char *argv[]){
 
-    int primos[N],
-	posible_primo = 3,
-	numero_primos = 1,
-	contador = 1;
-
-    primos[0] = 2;
-
-    do{
-	for(int divisor = 2; divisor < posible_primo; divisor++)
-	    if(posible_primo % divisor != 0){
-		primos[contador] = posible_primo;
-		numero_primos++;
-		contador++;
-	    }
-	posible_primo++;
-	
-    }while(numero_primos < N);
-
-    	for(int contador = 0; contador < N; contador++)
-	    printf("%i ", primos[contador]);
 
     printf("\n");
 
