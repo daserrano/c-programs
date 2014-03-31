f3b3138079b54c1631161b3fee7422846cec56dc
David <david@david-VirtualBox.(none)>
2014-03-31 13:09:02 +0200

    continous fraction did it

---
+++ solutions/17_continous_fraction/fraction.cpp
@@ -0,0 +1,39 @@
+/* Calculate the continous fraction of 2 with 10 levels of recursion.
+
+   1
+   ---  
+   n! +   1
+        -----
+        (n-1)!
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+
+
+int factorial(int n){
+    if(n <= 0)
+	return 1;
+
+    return n * factorial(n-1);
+
+}
+
+double e(int numeros){
+    if(numeros < 0)
+	return 0;
+
+    return 1. / factorial(numeros) + e(numeros - 1);
+
+}
+
+
+int main (int argc, char *argv[]){
+
+    printf("%lf \n", e(N));    
+
+    return EXIT_SUCCESS;
+}
