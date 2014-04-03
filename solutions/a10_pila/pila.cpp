af45526d8ccd3595a0ab3c89eda25bc7aef62edc
David <david@david-VirtualBox.(none)>
2014-04-04 01:10:46 +0200

    struct with pila

---
+++ solutions/a10_pila/pila.cpp
@@ -0,0 +1,106 @@
+/* Hacer una pila del tipo struct TPila o TStack.
+ *
+ *  Internamente tiene un array de MAX caracteres de tipo char
+ *
+ *  Tiene un entero que se llama cima o top, que siempre indica la primera posicion libre en el array.
+ *
+ *  La funcion show o muestra que muestra el contenido de la pila desde la base hasta la cima.
+ *  Tambien que reciba un objeto de TPila y lo muestra y no devuelve nada.
+ *
+ *  La funcion push va a recibir un caracter. Y va a recibir un objeto del TPila. Su objetivo es meter el caracter dentro de la pila. Si la pila 
+ *   esta llena no se puede introduccir nada.
+ *
+ *  La funcion pop devuelve el ultimo objeto apilado.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+enum TOpcion { muestra=1, escribir, ultimo, salir};
+
+struct TPila{
+
+    char caracteres[MAX];
+    int cima;
+
+};  
+
+enum TOpcion menu(){
+
+    int opcion_elegida;
+
+    printf("Elige una opcion: \n"
+	    "1. Mostrar pila. \n"
+	    "2. Escribir caracter. \n"
+	    "3. Mostrar ultimo caracter \n"
+	    "4. Salir \n"
+	  );
+    scanf(" %i", &opcion_elegida);
+
+    return (enum TOpcion) opcion_elegida;
+}
+
+void show(struct TPila pila){
+
+    for(int index= 0; index < pila.cima ; index++)
+	printf("%c ", pila.caracteres[index]);
+
+    printf("\n");
+
+}
+
+void push(struct TPila pila, char caracter){
+
+    // INCOMPLETE.
+
+}
+
+void pop(const char *puntero){
+
+}
+
+int main(int argc, char *argv[]){
+
+    struct TPila pila;
+    int opcion;
+    char caracter;
+
+    const char *puntero = 0;
+
+    pila.cima = 0;
+
+    do{
+	switch(opcion = menu()){
+	    case 1:
+		show(pila);
+		break;
+		
+	    case 2:
+		printf(" Introduce un caracter: ");
+		scanf(" %c", &caracter);
+
+		puntero = caracter; // It has an error.
+
+		push(pila, caracter); 
+
+		break;
+
+	    case 3:
+		pop(puntero);
+
+
+
+	}
+
+
+
+    }while( opcion != 3);
+
+
+    return EXIT_SUCCESS;
+
+}
