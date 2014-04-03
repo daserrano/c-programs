70ba4d4e9f3f4280209191bde8d4a83634fa64b0
David Serrano Indiano <dserrano@dam1-pc00.(none)>
2014-04-03 12:46:34 +0200

    statement of pila

---
+++ solutions/a_pila/pila.cpp
@@ -0,0 +1,25 @@
+/* Hacer una pila del tipo struct TPila o TStack.
+*
+*  Internamente tiene un array de MAX caracteres de tipo char
+*
+*  Tiene un entero que se llama cima o top, que siempre indica la primera posicion libre en el array.
+*
+*  La funcion show o muestra que muestra el contenido de la pila desde la base hasta la cima.
+*  Tambien que reciba un objeto de TPila y lo muestra y no devuelve nada.
+*
+*  La funcion push va a recibir un caracter. Y va a recibir un objeto del TPila. Su objetivo es meter el caracter dentro de la pila. Si la pila esta llena no se puede
+*   introduccir nada.
+*
+*  La funcion pop devuelve el ultimo objeto apilado.
+*
+*/
+
+
+int main(int argc, char *argv[]){
+
+
+
+
+   return EXIT_SUCCESS;
+} 
+
